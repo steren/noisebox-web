52ce2d99b97cd0df59a0ee6dc92afb499bfa22e0
Steren <steren.giannini@gmail.com>
2011-12-21 01:21:03 +0100

    box

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.configure(function() {
 var TagSchema = new mongoose.Schema({
     id : String
   , content: {}
-  , updated: {type: Date, default: Date.now}
+  , updated: Date
+  , created: {type: Date, default: Date.now}
 });
 var Tag = mongoose.model('Tag', TagSchema);
 
@@ -22,19 +23,35 @@ var Tag = mongoose.model('Tag', TagSchema);
 var BoxSchema = new mongoose.Schema({
     id: String
   , created: {type: Date, default: Date.now}
+  , latesttag: String
 });
 var Box = mongoose.model('Box', BoxSchema);
 
 mongoose.connect('mongodb://localhost/noisebox');
 
-
+/** Get a tag content
+ * @param id : id of the tag
+ * @param boxid : id of the box
+ */
 app.get('/tag/:id', function(req, res){
   if( req.param('id') ) {
-	  console.log(req.param('id'));
+	  console.log("GET Tag", req.param('id'));
 
-    Tag.findOne({id : req.param('id')}, function (err, doc) {
-      // delete doc._id;
-      res.send(doc);
+    Tag.findOne({id : req.param('id')}, function (err, tag) {
+      
+      // delete tag._id;
+      res.send(tag);
+
+      // create or update box
+      if(req.param('boxid')) {
+        Box.findOne({id : req.param('boxid')}, function (err, doc) {
+          var box = doc || new Box();
+          box.id = req.param('boxid');
+          box.latesttag = req.param('id');
+          box.save();
+          console.log("boxsaved");
+        });
+      }
     });
 
   } else {
@@ -42,9 +59,13 @@ app.get('/tag/:id', function(req, res){
   }
 });
 
+/** Set a tag content
+ * @param id : id of the tag
+ */
 app.post('/tag', function(req, res){
 
   if( req.param('id') ) {
+	  console.log("POST Tag", req.param('id'));
     Tag.findOne({id : req.param('id')}, function (err, doc) {
       var tag = doc || new Tag();
 
@@ -63,8 +84,20 @@ app.post('/tag', function(req, res){
 
 });
 
-app.get('/box', function(req, res){
+/** Get info abotu a box, including the last seen tag.
+ * @param id : id of the box
+ */
+app.get('/box/:id', function(req, res){
+  if( req.param('id') ) {
+	  console.log("GET Box", req.param('id'));
 
+    Box.findOne({id : req.param('id')}, function (err, doc) {
+      res.send(doc);
+    });
+
+  } else {
+    res.send(null);
+  }
 });
 
 app.listen(3000);
